a3c3fa73a2fdddb64f7b9a181f5f441ab4463957
ibrahimtuzer <orcawhite12@gmail.com>
2024-05-11 02:37:20 +0300

    faker data ile flatList kullanımı

--- src/screens/AboutScreen.js
+++ src/screens/AboutScreen.js
@@ -1,14 +1,52 @@
 // AboutScreen.js
 import React from 'react';
-import { View, Text, Button } from 'react-native';
+import { View, Text, Button, FlatList, Image, StyleSheet } from 'react-native';
+import { Images } from '../../fakerData';
 
 const AboutScreen = ({ navigation }) => {
+
+  // Resim öğelerini yuvarlak hale getiren işlev
+  const renderImageItem = ({ item }) => (
+    <View style={styles.imageContainer}>
+      <Image source={{ uri: item.image }} style={styles.image} />
+    </View>
+  );
+
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>About Screen</Text>
+    <View style={{ flex: 1 }}>
+      <FlatList
+        data={Images}
+        numColumns={2} // İki sütun olacak şekilde ayarlandı
+        contentContainerStyle={styles.flatListContainer}
+        renderItem={renderImageItem}
+        keyExtractor={item => item.id}
+      />
       <Button title="Go to Contact" onPress={() => navigation.navigate('Contact')} />
     </View>
   );
 }
+
+const imageSize = 140; // Sabit genişlik değeri
+
+const styles = StyleSheet.create({
+  flatListContainer: {
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  imageContainer: {
+    width: imageSize,
+    height: imageSize,
+    padding: 5,
+    aspectRatio: 1, // Resimlerin yuvarlak olmasını sağlamak için
+    borderRadius: imageSize / 2, // Yuvarlak border için
+    overflow: 'hidden', // Yuvarlak border için
+  },
+  image: {
+    flex: 1,
+    width: '100%',
+    aspectRatio: 1, // Resimlerin yuvarlak olmasını sağlamak için
+    borderRadius: imageSize / 2, // Yuvarlak border için
+  },
+});
 
 export default AboutScreen;
--- src/screens/ContactScreen.js
+++ src/screens/ContactScreen.js
@@ -1,13 +1,51 @@
-// ContactScreen.js
+import { faker } from '@faker-js/faker';
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, FlatList, Image, StyleSheet } from 'react-native';
+
+const DATA = Array(20)
+  .fill(null)
+  .map(() => ({
+    id: faker.string.uuid(),
+    avatar: faker.image.avatarLegacy(),
+    email: faker.internet.email()
+  }));
 
 const ContactScreen = () => {
-  return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Contact Screen</Text>
+  const renderItem = ({ item }) => (
+    <View style={styles.itemContainer}>
+      <Image source={{ uri: item.avatar }} style={styles.avatar} />
+      <Text style={styles.email}>{item.email}</Text>
     </View>
   );
-}
+
+  return (
+    <View style={{ flex: 1 }}>
+      <FlatList
+        data={DATA}
+        renderItem={renderItem}
+        keyExtractor={item => item.id}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  itemContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    padding: 10,
+    borderBottomWidth: 1,
+    borderBottomColor: '#ccc',
+  },
+  avatar: {
+    width: 50,
+    height: 50,
+    borderRadius: 25,
+    marginRight: 10,
+  },
+  email: {
+    fontSize: 16,
+  },
+});
 
 export default ContactScreen;
